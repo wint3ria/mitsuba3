c82da00a8c03016423a2bd254ee25918e81507ab
Nicolas Misk <nicolas.misk@gmail.com>
2026-02-18 17:27:15 +0100

    Handle all null weights case

--- src/eradiate_plugins/phase/multiphase.cpp
+++ src/eradiate_plugins/phase/multiphase.cpp
@@ -231,7 +231,7 @@ public:
             weight_sum += weight_values.back();
         }
         
-        Float inv_weight_sum = dr::rcp(weight_sum);
+        Float inv_weight_sum = dr::select(weight_sum < dr::Epsilon<Float>, 1.0f, dr::rcp(weight_sum));
 
         if (unlikely(ctx.component != (uint32_t) -1)) {
             PhaseFunctionContext ctx2(ctx);
